d2ce107d1ce49c23dc11fff48aa6913be765fa08
AdalbertoBenatoCordeiro <adalbertobcord@hotmail.com>
2021-03-19 17:55:44 -0300

    Primeiro Commit das aulas

--- aula04/exemplos/exemplo05/Robo.java
+++ aula04/exemplos/exemplo05/Robo.java
@@ -4,17 +4,18 @@ public class Robo {
     private int x, y; // restritos a class robo por estar com private
     private int maxX, maxY;
 
-    public Robo(int x, int y, int maxX, int maxY){
+    public Robo(int x, int y, int maxX, int maxY) {
         this.x = x;
         setY(y);
         this.maxX = maxX;
         this.maxY = maxY;
     }
-    public void norte() {
-       if (y < maxY) {
-        y++;
 
-       }
+    public void norte() {
+        if (y < maxY) {
+            y++;
+
+        }
     }
 
     public int getY() {
@@ -22,10 +23,9 @@ public class Robo {
     }
 
     public void setY(int novoY) {
-        if( novoY >= -maxY && novoY <= maxY) {
-            y = novoY;  
-        } 
-
+        if (novoY >= -maxY && novoY <= maxY) {
+            y = novoY;
+        }
 
     }
 
