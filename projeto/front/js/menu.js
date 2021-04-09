da331dff15f5ded89fbdd684e6b9e0857669755e
AdalbertoBenatoCordeiro <adalbertobcord@hotmail.com>
2021-04-09 10:10:04 -0300

    Projeto atualizado pela manha de 09 de abril com front end

--- projeto/front/js/menu.js
+++ projeto/front/js/menu.js
@@ -1,39 +1,67 @@
 function validaLogin() {
     let userTxt = localStorage.getItem("userlogged");
-
     if (!userTxt) {
         window.location = "index.html"
     }
-
     let user = JSON.parse(userTxt);
     document.getElementById("user").innerHTML = `nome: ${user.nome} RACF ${user.racf}`
     document.getElementById("foto").innerHTML = `<img src=${user.linkFoto}>`
-
 }
 function logout() {
     localStorage.removeItem("userlogged");
     window.location = "index.html"
 }
-function gerarRelatorio() {
-    //  let dataInicio = document.getElementById("dataInicio").value 
-    //  let dataFinal = document.getElementById("dataFinal").value 
+function gerarRelatorioEventos() {
+    let dataInicio = document.getElementById("dataInicio").value
+    let dataFinal = document.getElementById("dataFinal").value
 
-    fetch("http://localhost:8080/usuarios/all")
-        .then(res => tratarResposta(res))
-
-    console.log(dataInicio)
-    console.log(dataFinal)
-
+    let datas = {
+        dataIni: dataInicio,
+        dataFim: dataFinal
+    }
+    let msg = {
+        method: 'POST',
+        body: JSON.stringify(datas),
+        headers:{
+            'Content-type':'application/json'
+        }
+    }
+    fetch("http://localhost:8080/evento/data", msg)
+        .then(res => tratarRespostaEvento(res))
+   
 }
-function tratarResposta(resposta) {
+    function gerarRelatorioAlarmes() {
+        
+    fetch("http://localhost:8080/alarmes/all")
+        .then(res => tratarResposta(res))
+    
+}
+function tratarRespostaEvento(resposta) {
     if (resposta) {
-        resposta.json().then(res => exibirDados(res))
+        resposta.json().then(res => exibirDadosEventos(res))
     }
 }
-function exibirDados(listaUsers) {
-    let tabela = '<table class="table table-sm"> <tr> <th>nome</th> <th>email</th> <th>RACF</th> </tr>'
-    for (i = 0; i < listaUsers.length; i++) {
-        tabela = tabela + `<tr> <td>${listaUsers[i].nome}</td> <td>${listaUsers[i].email}</td> <td>${listaUsers[i].RACF}</td> </tr>`
+
+function tratarResposta(resposta) {
+    if (resposta) {
+        resposta.json().then(res => exibirDadosAlarmes(res))
+    }
+}
+
+function exibirDadosAlarmes(listaAlarmes) {
+    let tabela = '<table class="table table-sm"> <tr> <th>nome</th> <th>descricao</th> </tr>'
+    for (i = 0; i < listaAlarmes.length; i++) {
+        tabela = tabela + `<tr> <td>${listaAlarmes[i].nome}</td> <td>${listaAlarmes[i].descricao}</td> </tr>`
+    }
+    tabela = tabela + '</table>'
+    document.getElementById("tabela").innerHTML = tabela
+}
+function exibirDadosEventos(listaEventos) {
+    console.log(listaEventos);
+    let tabela = '<table class="table table-sm"> <tr> <th>data</th> <th>alarme</th> <th>equipamento</th>  </tr>'
+    for (i = 0; i < listaEventos.length; i++) {
+        let databr = new Date(listaEventos[i].dataevt).toLocaleDateString("pt-BR", { timeZone: 'UTC' });
+        tabela = tabela + `<tr> <td>${databr}</td>  <td>${listaEventos[i].alarme.nome}</td> <td>${listaEventos[i].equipamento.hostname}</td> </tr>`
     }
     tabela = tabela + '</table>'
     document.getElementById("tabela").innerHTML = tabela
